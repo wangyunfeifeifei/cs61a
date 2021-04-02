76d1594bf6d22e8eb8bfaf56f099820a52815f2b
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2021-04-02 20:10:20 +0800

    finish lab11

--- lab11/lab11.scm
+++ lab11/lab11.scm
@@ -3,4 +3,5 @@
 
 (define-macro (def func bindings body)
     'YOUR-CODE-HERE
+    `(define ,func (lambda ,bindings ,body))
 )
--- lab11/tests/wwsd-macros.py
+++ lab11/tests/wwsd-macros.py
@@ -7,74 +7,56 @@ test = {
         {
           'code': r"""
           scm> (define-macro (f x) (car x))
-          8be72d11934954a354707b9d4b8d7f0d
-          # locked
+          f
           scm> (f (2 3 4)) ; type SchemeError for error, or Nothing for nothing
-          805a87056a1a3fd559e4ef12a815b2be
-          # locked
+          2
           scm> (f (+ 2 3))
-          ace42d2542d5f71f0f94b8c743a02749
-          # locked
+          #[+]
           scm> (define x 2000)
-          5ce45267887fa5dae1771a9b64b54929
-          # locked
+          x
           scm> (f (x y z))
-          750fc0b00d0da9fbf3959a00bf507d75
-          # locked
+          2000
           scm> (f (list 2 3 4))
-          e2cac60edcda053657350d039a6cb9f9
-          # locked
+          #[list]
           scm> (f (quote (2 3 4)))
-          c6d536c5acab2b28e563f34c64535296
-          # locked
+          SchemeError
           scm> (define quote 7000)
-          e0a688960a27713f8b95c7744ee2f654
-          # locked
+          quote
           scm> (f (quote (2 3 4)))
-          dcbb0b3d6dfbacc4b0c170c3b0bd2fd8
-          # locked
+          7000
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
           scm> (define-macro (g x) (+ x 2))
-          8d7ecccd30c7d582c4d72356ab083496
-          # locked
+          g
           scm> (g 2)
-          3cfd97a152be55d1a3486dbacb2bf637
-          # locked
+          4
           scm> (g (+ 2 3))
-          c6d536c5acab2b28e563f34c64535296
-          # locked
+          SchemeError
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
           scm> (define-macro (if-else-5 condition consequent) `(if ,condition ,consequent 5))
-          202039864acea4eb3298dfda57e4c472
-          # locked
+          if-else-5
           scm> (if-else-5 #t 2)
-          805a87056a1a3fd559e4ef12a815b2be
-          # locked
+          2
           scm> (if-else-5 #f 3)
-          9934e055a74f1f7f5fb94c0f9fd6402d
-          # locked
+          5
           scm> (if-else-5 #t (/ 1 0))
-          c6d536c5acab2b28e563f34c64535296
-          # locked
+          SchemeError
           scm> (if-else-5 #f (/ 1 0))
-          9934e055a74f1f7f5fb94c0f9fd6402d
-          # locked
+          5
           scm> (if-else-5 (= 1 1) 2)
-          805a87056a1a3fd559e4ef12a815b2be
-          # locked
+          2
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         }
       ],
       'scored': True,
